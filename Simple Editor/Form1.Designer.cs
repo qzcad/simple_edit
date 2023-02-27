57212b196209c896c51118423631d42a8317c901
Serhii Choporov <s.choporoff@gmail.com>
2023-02-27 16:00:31 +0200

    save to file

--- Simple Editor/Form1.Designer.cs
+++ Simple Editor/Form1.Designer.cs
@@ -28,90 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             mainMenu = new MenuStrip();
             fileToolStripMenuItem = new ToolStripMenuItem();
             newToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator2 = new ToolStripSeparator();
             openToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator4 = new ToolStripSeparator();
             saveToolStripMenuItem = new ToolStripMenuItem();
             saveasToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator3 = new ToolStripSeparator();
             exitToolStripMenuItem = new ToolStripMenuItem();
-            richTextBox1 = new RichTextBox();
             mainToolBar = new ToolStrip();
+            toolStripButtonNew = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
             mainStatusBar = new StatusStrip();
             openFileDialog = new OpenFileDialog();
             saveFileDialog = new SaveFileDialog();
             fontDialog = new FontDialog();
+            textEditor = new RichTextBox();
             mainMenu.SuspendLayout();
+            mainToolBar.SuspendLayout();
             SuspendLayout();
             // 
             // mainMenu
             // 
+            mainMenu.ImageScalingSize = new Size(20, 20);
             mainMenu.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
             mainMenu.Location = new Point(0, 0);
             mainMenu.Name = "mainMenu";
-            mainMenu.Size = new Size(800, 24);
+            mainMenu.Padding = new Padding(7, 3, 0, 3);
+            mainMenu.Size = new Size(914, 30);
             mainMenu.TabIndex = 0;
             mainMenu.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
-            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, openToolStripMenuItem, saveToolStripMenuItem, saveasToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, toolStripSeparator2, openToolStripMenuItem, toolStripSeparator4, saveToolStripMenuItem, saveasToolStripMenuItem, toolStripSeparator3, exitToolStripMenuItem });
             fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Size = new Size(46, 24);
             fileToolStripMenuItem.Text = "&File";
             // 
             // newToolStripMenuItem
             // 
             newToolStripMenuItem.Name = "newToolStripMenuItem";
-            newToolStripMenuItem.Size = new Size(180, 22);
+            newToolStripMenuItem.Size = new Size(141, 26);
             newToolStripMenuItem.Text = "&New";
+            newToolStripMenuItem.Click += newToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(138, 6);
             // 
             // openToolStripMenuItem
             // 
             openToolStripMenuItem.Name = "openToolStripMenuItem";
-            openToolStripMenuItem.Size = new Size(180, 22);
+            openToolStripMenuItem.Size = new Size(141, 26);
             openToolStripMenuItem.Text = "&Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator4
+            // 
+            toolStripSeparator4.Name = "toolStripSeparator4";
+            toolStripSeparator4.Size = new Size(138, 6);
             // 
             // saveToolStripMenuItem
             // 
             saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            saveToolStripMenuItem.Size = new Size(180, 22);
+            saveToolStripMenuItem.Size = new Size(141, 26);
             saveToolStripMenuItem.Text = "&Save";
             // 
             // saveasToolStripMenuItem
             // 
             saveasToolStripMenuItem.Name = "saveasToolStripMenuItem";
-            saveasToolStripMenuItem.Size = new Size(180, 22);
+            saveasToolStripMenuItem.Size = new Size(141, 26);
             saveasToolStripMenuItem.Text = "Save &as";
+            // 
+            // toolStripSeparator3
+            // 
+            toolStripSeparator3.Name = "toolStripSeparator3";
+            toolStripSeparator3.Size = new Size(138, 6);
             // 
             // exitToolStripMenuItem
             // 
             exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            exitToolStripMenuItem.Size = new Size(180, 22);
+            exitToolStripMenuItem.Size = new Size(141, 26);
             exitToolStripMenuItem.Text = "&Exit";
             exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
             // 
-            // richTextBox1
-            // 
-            richTextBox1.Location = new Point(107, 53);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(424, 219);
-            richTextBox1.TabIndex = 1;
-            richTextBox1.Text = "";
-            // 
             // mainToolBar
             // 
-            mainToolBar.Location = new Point(0, 24);
+            mainToolBar.ImageScalingSize = new Size(20, 20);
+            mainToolBar.Items.AddRange(new ToolStripItem[] { toolStripButtonNew, toolStripSeparator1 });
+            mainToolBar.Location = new Point(0, 30);
             mainToolBar.Name = "mainToolBar";
-            mainToolBar.Size = new Size(800, 25);
+            mainToolBar.Size = new Size(914, 27);
             mainToolBar.TabIndex = 2;
             mainToolBar.Text = "toolStrip1";
             // 
+            // toolStripButtonNew
+            // 
+            toolStripButtonNew.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripButtonNew.Image = (Image)resources.GetObject("toolStripButtonNew.Image");
+            toolStripButtonNew.ImageTransparentColor = Color.Magenta;
+            toolStripButtonNew.Name = "toolStripButtonNew";
+            toolStripButtonNew.Size = new Size(43, 24);
+            toolStripButtonNew.Text = "New";
+            toolStripButtonNew.TextImageRelation = TextImageRelation.TextAboveImage;
+            toolStripButtonNew.Click += newToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 27);
+            // 
             // mainStatusBar
             // 
-            mainStatusBar.Location = new Point(0, 428);
+            mainStatusBar.ImageScalingSize = new Size(20, 20);
+            mainStatusBar.Location = new Point(0, 578);
             mainStatusBar.Name = "mainStatusBar";
-            mainStatusBar.Size = new Size(800, 22);
+            mainStatusBar.Padding = new Padding(1, 0, 16, 0);
+            mainStatusBar.Size = new Size(914, 22);
             mainStatusBar.TabIndex = 3;
             mainStatusBar.Text = "statusStrip1";
             // 
@@ -119,20 +157,34 @@
             // 
             openFileDialog.FileName = "openFileDialog1";
             // 
+            // textEditor
+            // 
+            textEditor.Dock = DockStyle.Fill;
+            textEditor.Location = new Point(0, 57);
+            textEditor.Margin = new Padding(3, 4, 3, 4);
+            textEditor.Name = "textEditor";
+            textEditor.Size = new Size(914, 521);
+            textEditor.TabIndex = 4;
+            textEditor.Text = "";
+            textEditor.TextChanged += textEditor_TextChanged;
+            // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(textEditor);
             Controls.Add(mainStatusBar);
             Controls.Add(mainToolBar);
-            Controls.Add(richTextBox1);
             Controls.Add(mainMenu);
             MainMenuStrip = mainMenu;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "MainForm";
             Text = "Simple Edit";
             mainMenu.ResumeLayout(false);
             mainMenu.PerformLayout();
+            mainToolBar.ResumeLayout(false);
+            mainToolBar.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -146,11 +198,16 @@
         private ToolStripMenuItem saveToolStripMenuItem;
         private ToolStripMenuItem saveasToolStripMenuItem;
         private ToolStripMenuItem exitToolStripMenuItem;
-        private RichTextBox richTextBox1;
         private ToolStrip mainToolBar;
         private StatusStrip mainStatusBar;
         private OpenFileDialog openFileDialog;
         private SaveFileDialog saveFileDialog;
         private FontDialog fontDialog;
+        private RichTextBox textEditor;
+        private ToolStripButton toolStripButtonNew;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripSeparator toolStripSeparator4;
+        private ToolStripSeparator toolStripSeparator3;
     }
 }
--- Simple Editor/Form1.cs
+++ Simple Editor/Form1.cs
@@ -2,14 +2,80 @@ namespace Simple_Editor
 {
     public partial class MainForm : Form
     {
+        private string filePath;
+        private bool textChanged;
+
         public MainForm()
         {
             InitializeComponent();
+
+            this.filePath = "";
+            this.textChanged = false;
+        }
+
+        private void WriteText(string filePath)
+        {
+            this.filePath = filePath;
+            System.IO.File.WriteAllText(this.filePath, this.textEditor.Text);
+            this.textChanged = false;
+        }
+
+        private void SaveAs()
+        {
+            if (this.saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.WriteText(this.saveFileDialog.FileName);
+            }
+        }
+
+        private void Save()
+        {
+            if (this.filePath == null || this.filePath == "")
+            {
+                this.SaveAs();
+            }
+            else
+            {
+                this.WriteText(this.filePath);
+            }
+        }
+
+        private void SaveChanges()
+        {
+            if (this.textChanged) 
+            { 
+                if (MessageBox.Show("Save changes?", "Text is changed", MessageBoxButtons.YesNo) == DialogResult.Yes) 
+                {
+                    this.Save();
+                }
+            }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            this.SaveChanges();
             this.Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.textEditor.Clear();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                this.filePath = this.openFileDialog.FileName;
+                string text = System.IO.File.ReadAllText(this.filePath);
+                this.textEditor.Text = text;
+                this.textChanged = false;
+            }
+        }
+
+        private void textEditor_TextChanged(object sender, EventArgs e)
+        {
+            this.textChanged = true;
         }
     }
 }
